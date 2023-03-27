159b7a1089ab7ea26594818cbd8b3b9da6616a56
seungwonkim <seungwonk@megazone.com>
2023-03-27 15:08:29 +0900

    Test 8th Commit

--- main.tf
+++ main.tf
@@ -14,12 +14,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
@@ -27,7 +27,7 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "container" {
+resource "azurerm_storage_container" "example" {
   name                  = var.container_name
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
