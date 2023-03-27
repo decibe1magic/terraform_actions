7a8950a4642ff9bca564ed07ec287bb2b950af92
seungwonkim <seungwonk@megazone.com>
2023-03-27 13:24:27 +0900

    Test 1st Commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+# Terraform Cloud
+terraform {
+  cloud {
+    organization = "MZC-ORG"
+
+    workspaces {
+      name = "MZC-MTC-swkim-ghdemo"
+    }
+  }
+}
+
+# main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
